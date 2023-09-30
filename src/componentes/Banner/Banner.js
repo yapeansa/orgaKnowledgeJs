7b6eb86d9cd0afc22d3f48365db875273c9c9846
Yago <yago.pereira@estudante.ufjf.br>
2023-09-30 00:32:28 -0300

    Alterações de estilo.

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,7 +3,14 @@ import './Banner.css';
 export const Banner = () => {
     return (
         <header className="banner">
-            <img src="imagens/banner.png" alt="O banner principal da página do Organo" />
+            <div className="left">
+                <img src="imagens/Logo.svg" alt="Logo da Organo" />
+                <h2>As Maiores Mentes</h2>
+                <p>reunidas em um só lugar!</p>
+            </div>
+            <div className="right">
+                <img src="imagens/img_header.jpg" alt="Destaque" />
+            </div>
         </header>
     );
 }
