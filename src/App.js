8995cd7890ab8d8e5feba2e25df8e5e73fb26ee3
Yago <yago.pereira@estudante.ufjf.br>
2023-10-01 10:33:22 -0300

    Alteração na sintaxe.

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
 
   const [exibir, setExibir] = useState(true);
 
-  function toggleForm () {
+  const toggleForm = () => {
     let controle = exibir;
     setExibir(!controle);
   }
